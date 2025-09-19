775cf296dd01c9030e2dfacdff772d3705151d37
Temesgen <temesgenabdissa2@gmail.coom>
2025-09-19 03:56:39 +0300

    update product files

--- front-end/src/Componets/Product/Product.jsx
+++ front-end/src/Componets/Product/Product.jsx
@@ -1,62 +1,48 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import classes from "./Product.module.css";
-import ProductCard from "./ProductCard";
-import Loader from "../Loader/Loader";
 
-const Product = () => {
-  const [products, setProducts] = useState([]);
+import classes from "./Product.module.css";
+import Loader from "../Loader/Loader";
+import ProductCard from "./ProductCard";
+import { productUrl } from "../../Api/EndPoint";
+
+function Product() {
+  const [products, setProducts] = useState();
   const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
 
   useEffect(() => {
-    const fetchProducts = async () => {
-      setIsLoading(true);
-      setError(null);
-      try {
-        // Optional delay to make loader visible for testing
-        // await new Promise((resolve) => setTimeout(resolve, 500));
-
-        const res = await axios.get("https://fakestoreapi.com/products");
+    setIsLoading(true);
+    axios
+      .get(`${productUrl}/products`)
+      .then((res) => {
         setProducts(res.data);
-      } catch (err) {
-        console.error(err);
-        setError("Failed to load products. Please try again.");
-      } finally {
         setIsLoading(false);
-      }
-    };
-
-    fetchProducts();
+      })
+      .catch((err) => {
+        console.log(err);
+        setIsLoading(false);
+      });
   }, []);
 
   return (
     <>
-      {isLoading && <Loader />}
-      {error && (
-        <p style={{ color: "red", textAlign: "center", padding: "20px" }}>
-          {error}
-        </p>
-      )}
-      {!isLoading && !error && (
-        <section className={classes.product__container}>
-          {products.length > 0 ? (
-            products.map((singleProduct) => (
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <section className={classes.products_container}>
+          {products?.map((singleProduct) => {
+            return (
               <ProductCard
                 renderAdd={true}
                 product={singleProduct}
                 key={singleProduct.id}
               />
-            ))
-          ) : (
-            <p style={{ textAlign: "center", padding: "20px" }}>
-              No products found.
-            </p>
-          )}
+            );
+          })}
         </section>
       )}
     </>
   );
-};
+}
 
 export default Product;
