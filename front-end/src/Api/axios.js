ebd151988da7ae2320318713cd287cc5f60c4f1a
Temesgen <temesgenabdissa2@gmail.coom>
2025-09-26 01:14:09 +0300

    adding backend url at axios

--- front-end/src/Api/axios.js
+++ front-end/src/Api/axios.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 const axiosInstance = axios.create({
-  //base ul of  th emy amazon  backend functions API 
-  baseURL: "http://127.0.0.1:5001/clone-20b7f/us-central1/api",
+  //base ul of  th emy amazon  backend functions API
+  // baseURL: "http://127.0.0.1:5001/clone-20b7f/us-central1/api",
   // base ul of amazon backend on render.com
-  //   baseURL: "https://amazon-api-deploy-1-qtc6.onrender.com",
+  baseURL: "https://api-hwrsrsgpw-temesgenabdissas-projects.vercel.app/",
 });
 
 export { axiosInstance };
