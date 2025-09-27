779e5e087854c9dfa8987f1a1c93ac0159e95713
Temesgen <temesgenabdissa2@gmail.coom>
2025-09-28 01:34:08 +0300

    updating the header

--- front-end/src/Componets/Header/LowerHeader.jsx
+++ front-end/src/Componets/Header/LowerHeader.jsx
@@ -1,26 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import { AiOutlineMenu } from "react-icons/ai";
 import classes from "./Header.module.css";
 
 function LowerHeader() {
+  const [open, setOpen] = useState(false);
+
+  const toggleMenu = () => setOpen(!open);
+
+  const goHome = () => {
+    window.location.href = "/";
+  };
+
   return (
     <div className={classes.lower__container}>
       <ul>
-        <li>
+        {/* Toggle button (always visible) */}
+        <li onClick={toggleMenu} style={{ cursor: "pointer" }}>
           <AiOutlineMenu />
-
           <p>All</p>
         </li>
-        <li>Amazon haul</li>
-        <li>Best Sellers</li>
-        <li>New releases</li>
-        <li>Amazon Basics</li>
-        <li>Today's Deal</li>
-        <li>Books</li>
-        <li>Shopper Toolkit</li>
-        <li>Fashion</li>
-        <li>Gift Cards</li>
-        
+
+        {/* Menu items */}
+        <div
+          className={`${classes.menu__items} ${open ? classes.menu__open : ""}`}
+        >
+          <li onClick={goHome}>Amazon haul</li>
+          <li onClick={goHome}>Best Sellers</li>
+          <li onClick={goHome}>New releases</li>
+          <li onClick={goHome}>Amazon Basics</li>
+          <li onClick={goHome}>Today's Deal</li>
+          <li onClick={goHome}>Books</li>
+          <li onClick={goHome}>Shopper Toolkit</li>
+          <li onClick={goHome}>Fashion</li>
+          <li onClick={goHome}>Gift Cards</li>
+        </div>
       </ul>
     </div>
   );
